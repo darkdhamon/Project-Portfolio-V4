cca264dcc1377b7560481a6c8bf1999178267824
Bronze Brown <dhamon.nightstar@gmail.com>
2021-01-28 11:32:42 -0500

    Technology rename to Tag #7

---
+++ Project Portfolio V4/Project Portfolio Domain/Model/Project/Tag.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Project_Portfolio_Domain.Repository.Abstract;
+
+namespace Project_Portfolio_Domain.Model.Project
+{
+    public class Tag : Entity
+    {
+        public string Name { get; set; }
+    }
+}
