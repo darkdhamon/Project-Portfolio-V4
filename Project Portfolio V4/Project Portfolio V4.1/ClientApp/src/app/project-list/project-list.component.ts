618f92edcd75f234e59e7840d7fb1421531dfb8d
Endrophes <Endrophes@users.noreply.github.com>
2021-02-02 20:10:52 -0500

    Added spacing of content
    Using justify-content to evenly space out content.

--- Project Portfolio V4/Project Portfolio V4.1/ClientApp/src/app/project-list/project-list.component.ts
+++ Project Portfolio V4/Project Portfolio V4.1/ClientApp/src/app/project-list/project-list.component.ts
@@ -19,7 +19,8 @@ export class ProjectListComponent implements OnInit {
   ngOnInit(): void {
     ///Make call to DB for porject lists
     this.http.get<Project[]>("api/Project/page/1").subscribe(data => {
-      this.projects = data;
+      ///For testing, I'm duplicating the data returned from core
+      this.projects = data;//[...data, ...data, ...data];
       console.log("ProjectListComponent - data:", data);
     });
   }
