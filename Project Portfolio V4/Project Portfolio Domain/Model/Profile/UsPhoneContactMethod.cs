4638260977da9cc006751330f0c99b5e1efdaa9a
Bronze Brown <dhamon.nightstar@gmail.com>
2021-02-04 04:16:52 -0500

    #7
    Model for Dynamic Content
    Model for Profile and ContactMethods

---
+++ Project Portfolio V4/Project Portfolio Domain/Model/Profile/UsPhoneContactMethod.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project_Portfolio_Domain.Model.Profile
+{
+    [Table("ContactMethod")]
+    public class UsPhoneContactMethod : ContactMethod
+    {
+        [Phone]
+        public string Number { get; set; }
+    }
+}
